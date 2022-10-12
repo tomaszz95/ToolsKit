270a945c8154fe56f199748e0424deb1599e7f8e
Tomasz Ż <tzuber95@gmail.com>
2022-10-12 19:17:05 +0200

    added todo in html and css

--- src/js/_switchThemes.js
+++ src/js/_switchThemes.js
@@ -5,17 +5,23 @@ export const switchPageThemes = () => {
 	let theme = localStorage.getItem('theme')
 
 	const switchColorToDark = () => {
-		root.style.setProperty('--header-color', 'rgb(19, 10, 10)')
-		root.style.setProperty('--element-color', 'rgb(29, 25, 25)')
+		root.style.setProperty('--primary-color', 'rgb(9, 5, 5)')
+		root.style.setProperty('--element-color', 'rgb(65, 58, 58)')
 		root.style.setProperty('--text-color', 'rgb(241, 236, 236)')
-		root.style.setProperty('--active-color', 'rgb(51, 202, 14)')
+		root.style.setProperty('--highlight-color', 'rgb(51, 202, 14)')
+		root.style.setProperty('--alert-color', 'rgb(255, 1, 1)')
+		root.style.setProperty('--check-color', 'rgb(59, 253, 11)')
+		root.style.setProperty('--edit-color', 'rgb(4, 216, 254)')
 	}
-
+	
 	const switchColorToLight = () => {
-		root.style.setProperty('--header-color', 'rgb(248, 248, 248)')
-		root.style.setProperty('--element-color', 'rgb(233, 233, 234)')
+		root.style.setProperty('--primary-color', 'rgb(251, 251, 251)')
+		root.style.setProperty('--element-color', 'rgb(223, 226, 229)')
 		root.style.setProperty('--text-color', 'rgb(19, 10, 10)')
-		root.style.setProperty('--active-color', 'rgb(232, 13, 13)')
+		root.style.setProperty('--highlight-color', 'rgb(216, 7, 7)')
+		root.style.setProperty('--alert-color', 'rgb(216, 7, 7)')
+		root.style.setProperty('--check-color', 'rgb(4, 99, 7)')
+		root.style.setProperty('--edit-color', 'rgb(134, 7, 189)')
 	}
 
 	const switchColorFromCookies = () => {
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import { switchPageThemes } from './_switchThemes.js'
+const navContainer = document.querySelector('.nav__container')
 
+const switchNavSelected = e => {
+    for (const btn of navContainer.children) {
+        btn.classList.remove('nav__container--item-selected')
+    }
+	e.target.closest('button').classList.add('nav__container--item-selected')
+}
+
+navContainer.addEventListener('click', switchNavSelected)
 switchPageThemes()
