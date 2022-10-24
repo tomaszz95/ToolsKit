8e164f6e6c8da61e53b18622760e4d08459c6b6f
Tomasz Ż <tzuber95@gmail.com>
2022-10-24 20:33:18 +0200

    fighting with calculator

--- src/js/_calculator.js
+++ src/js/_calculator.js
@@ -1,108 +1,69 @@
 export const calculatorFunction = () => {
 	const calculatorApp = document.querySelector('.calculator')
 	const calculatorDisplayPrevValue = document.querySelector('.calculator__display-prev')
-	const calculatorDisplayActuallValue = document.querySelector('.calculator__display-active')
+	const calculatorDisplayCurrentValue = document.querySelector('.calculator__display-active')
 	const calculatorBox = document.querySelector('.calculator__box')
-	let clickedValue
-	let prevValue
+	let currentValue = ''
+	let prevValue = ''
+	let chosenOperation = ''
+
 	// WRITE ON DISPLAY
-	const writeNumbersOnDisplay = e => {
-		if (clickedValue == undefined) {
-			clickedValue = e.target.dataset.value || e.key
-			calculatorDisplayActuallValue.textContent = clickedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
-		} else if (clickedValue.length < 16 && e.target.dataset.value != undefined) {
-			clickedValue = `${prevValue}${e.target.dataset.value}`
-			calculatorDisplayActuallValue.textContent = clickedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
-		} else if (clickedValue.length < 16 && e.key == '.') {
-			clickedValue = `${prevValue},`
-			calculatorDisplayActuallValue.textContent = clickedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
-		} else if (clickedValue.length < 16 && e.key != undefined) {
-			clickedValue = `${prevValue}${e.key}`
-			calculatorDisplayActuallValue.textContent = clickedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
-		}
-		
-		prevValue = clickedValue
-	}
-
-	// MANAGE CALCULATOR BUTTONS
-	const manageCalculatorItems = e => {
-		if (e.target.dataset.value >= 0 && e.target.dataset.value < 10) {
-			writeNumbersOnDisplay(e)
-		} else if (e.target.dataset.value == 'Backspace') {
-			console.log('Backspace')
-		} else if (e.target.dataset.value == 'Del') {
-			console.log('Del')
-		} else if (e.target.dataset.value == 'DelAll') {
-			console.log('DelAll')
-		} else if (e.target.dataset.value == 'Enter') {
-			console.log('Enter')
-		} else if (e.target.dataset.value == '+') {
-			console.log('+')
-		} else if (e.target.dataset.value == '-') {
-			console.log('-')
-		} else if (e.target.dataset.value == '*') {
-			console.log('*')
-		} else if (e.target.dataset.value == '/') {
-			console.log('/')
-		} else if (e.target.dataset.value == 'Fraction') {
-			console.log('Fraction')
-		} else if (e.target.dataset.value == 'Squared') {
-			console.log('Squared')
-		} else if (e.target.dataset.value == 'Root') {
-			console.log('Root')
-		} else if (e.target.dataset.value == 'Inverse') {
-			console.log('Inverse')
-		} else if (prevValue != undefined && e.target.dataset.value == ',' && prevValue.indexOf(',') < 0) {
-			writeNumbersOnDisplay(e)
-		} else if (e.target.dataset.value == 'e') {
-			console.log('e')
-		}
-	}
-
-	// MANAGE KEYS
-	const manageKeys = e => {
-		if (!calculatorApp.classList.contains('active-app')) return
-
-		if (e.key >= 0 && e.key < 10) {
-			writeNumbersOnDisplay(e)
-		} else if (e.key == 'Enter' || e.key == '=') {
-			console.log('=')
-			const clickedBtn = document.querySelector(`[data-value='Enter']`)
-			clickedBtn.classList.add('active')
-			setTimeout(() => {
-				clickedBtn.classList.remove('active')
-			}, 200)
-			return
-		} else if (e.key == 'Backspace') {
-			console.log('Backspace')
-		} else if (e.key == '+') {
-			console.log('+')
-		} else if (e.key == '-') {
-			console.log('-')
-		} else if (e.key == '*') {
-			console.log('*')
-		} else if (e.key == '/') {
-			console.log('/')
-		} else if (prevValue != undefined && (e.key == '.' || e.key == ',') && prevValue.indexOf(',') < 0) {
-			writeNumbersOnDisplay(e)
-
-			const clickedBtn = document.querySelector(`[data-value=',']`)
-			clickedBtn.classList.add('active')
-			setTimeout(() => {
-				clickedBtn.classList.remove('active')
-			}, 200)
-			return
+	const writeOnDisplay = () => {
+		calculatorDisplayCurrentValue.textContent = currentValue
+		console.log(currentValue);
+		if (chosenOperation == '') {
+			calculatorDisplayPrevValue.textContent = ''
 		} else {
-			return
+			calculatorDisplayPrevValue.textContent = prevValue + chosenOperation
 		}
-
-		const clickedBtn = document.querySelector(`[data-value='${e.key}']`)
-		clickedBtn.classList.add('active')
-		setTimeout(() => {
-			clickedBtn.classList.remove('active')
-		}, 200)
 	}
 
-	calculatorBox.addEventListener('click', manageCalculatorItems)
-	document.addEventListener('keyup', manageKeys)
+	// CHOSEN OPERATION
+	const chosenOperationFunction = (value, type) => {
+		if (currentValue == '') return
+
+		if (type == 'Operation' && prevValue != '') {
+			makeAnOperation(value)
+		} else if (type == 'Operation' && prevValue == '') {
+			chosenOperation = value
+			prevValue = currentValue
+			currentValue = ''
+		} 
+	}
+
+	// MANAGE CLICK
+	const manageCalculatorClick = e => {
+		if (!calculatorApp.classList.contains('active-app')) return
+		parseInt(currentValue, 10)
+
+		const clickedItemVDatasetValue = e.target.dataset.value
+		const clickedItemDatasetType = e.target.dataset.type
+
+		if (clickedItemDatasetType == 'Number') {
+			currentValue = currentValue.toString() + clickedItemVDatasetValue.toString()
+		} else if (clickedItemDatasetType == 'Solo') {
+			makeAnOperation(clickedItemVDatasetValue)
+		} else if (clickedItemVDatasetValue == ',' && !currentValue.includes(',') && currentValue !== '') {
+			currentValue = currentValue.toString() + clickedItemVDatasetValue.toString()
+		} else if (clickedItemVDatasetValue == 'Backspace' || e.target.parentElement.dataset.value == 'Backspace') {
+			currentValue = currentValue.toString().slice(0, -1)
+		} else if (clickedItemVDatasetValue == 'Del') {
+			currentValue = ''
+		} else if (clickedItemVDatasetValue == 'DelAll') {
+			currentValue = ''
+			prevValue = ''
+			chosenOperation = ''
+		} else if (clickedItemVDatasetValue == '=') {
+			makeAnOperation(clickedItemVDatasetValue)
+		}
+
+		chosenOperationFunction(clickedItemVDatasetValue, clickedItemDatasetType)
+		writeOnDisplay()
+	}
+
+	// OPERATIONS
+	const makeAnOperation = value => {}
+
+	calculatorBox.addEventListener('click', manageCalculatorClick)
+	// document.addEventListener('keyup', manageKeys)
 }
