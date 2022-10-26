334193f5ac3f464d232e2e2d386fd2c581358c3e
Tomasz Ż <tzuber95@gmail.com>
2022-10-26 15:18:40 +0200

    ended calculator

--- src/js/_calculator.js
+++ src/js/_calculator.js
@@ -3,10 +3,10 @@ export const calculatorFunction = () => {
 	const calculatorDisplayPrevValue = document.querySelector('.calculator__display-prev')
 	const calculatorDisplayCurrentValue = document.querySelector('.calculator__display-active')
 	const calculatorBox = document.querySelector('.calculator__box')
-	let currentValue = ''
-	let prevValue = ''
-	let chosenOperation = ''
-	let result = ''
+	let currentValue = '',
+		prevValue = '',
+		chosenOperation = '',
+		endResult = ''
 
 	// WRITE ON DISPLAY
 	const writeOnDisplay = () => {
@@ -37,18 +37,24 @@ export const calculatorFunction = () => {
 		}
 	}
 
-	// MANAGE CLICK, OPTIONS LIKE DEL, DELLALL, ENTER
+	// MANAGE CLICK, MANAGE OPTIONS LIKE DEL, DELLALL, ENTER
 	const manageCalculatorClick = e => {
 		if (!calculatorApp.classList.contains('active-app')) return
-
 		const clickedItemVDatasetValue = e.target.dataset.value
 		const clickedItemDatasetType = e.target.dataset.type
 
+		// BLOCK ONLY ZEROS IN CALC
+		if (currentValue == '0' && clickedItemVDatasetValue == '0') {
+			return
+		} else if (currentValue == '0' && clickedItemDatasetType == 'Number') {
+			currentValue = ''
+		}
+
 		currentValue = currentValue.toString()
 
-		if (clickedItemDatasetType == 'Number' && currentValue.length < 16 && currentValue !== "Can't divide by 0!") {
-			currentValue == result ? (currentValue = '') : false
-			currentValue = currentValue.toString() + clickedItemVDatasetValue.toString()
+		if (clickedItemDatasetType == 'Number' && currentValue.length < 15 && currentValue !== "Can't divide by 0!") {
+			currentValue == endResult ? (currentValue = '') : false
+			currentValue = currentValue + clickedItemVDatasetValue.toString()
 		} else if (currentValue != '' && clickedItemDatasetType == 'Solo') {
 			makeAnOperation(clickedItemVDatasetValue)
 		} else if (
@@ -57,9 +63,12 @@ export const calculatorFunction = () => {
 			currentValue !== '' &&
 			currentValue !== "Can't divide by 0!"
 		) {
-			currentValue = currentValue.toString() + clickedItemVDatasetValue.toString()
-		} else if (clickedItemVDatasetValue == 'Backspace' || e.target.parentElement.dataset.value == 'Backspace') {
-			currentValue = currentValue.toString().slice(0, -1)
+			currentValue = currentValue + clickedItemVDatasetValue.toString()
+		} else if (
+			(clickedItemVDatasetValue == 'Backspace' || e.target.parentElement.dataset.value == 'Backspace') &&
+			currentValue !== "Can't divide by 0!"
+		) {
+			currentValue = currentValue.slice(0, -1)
 		} else if (clickedItemVDatasetValue == 'Del') {
 			currentValue = ''
 		} else if (clickedItemVDatasetValue == 'DelAll') {
@@ -76,12 +85,10 @@ export const calculatorFunction = () => {
 
 	// OPERATIONS
 	const makeAnOperation = value => {
-		result = ''
+		let result = ''
+		endResult = ''
 		const prevNumberValue = parseFloat(prevValue)
 		const currentNumberValue = parseFloat(currentValue)
-		console.log(currentValue, prevValue)
-		if ((prevNumberValue == '' && isNaN(prevNumberValue)) || isNaN(currentNumberValue)) return
-		console.log(prevNumberValue, currentNumberValue)
 
 		switch (chosenOperation) {
 			case '+':
@@ -123,12 +130,68 @@ export const calculatorFunction = () => {
 				break
 		}
 
-		result = result.toString().slice(0, 18)
-		currentValue = result
+		if (result == "Can't divide by 0!") {
+			endResult = result
+		} else {
+			endResult = Math.round((result + Number.EPSILON) * 100) / 100
+		}
+		currentValue = endResult
 		prevValue = ''
 		chosenOperation = ''
 	}
 
+	// MANAGE KEYS
+	const manageKeys = e => {
+		if (!calculatorApp.classList.contains('active-app')) return
+		let keysItemVDatasetValue = e.key
+		let keysClickedItem
+
+		// DOWNLOAD KEY PUSHED ITEM
+		if (keysItemVDatasetValue == 'Enter') {
+			keysClickedItem = document.querySelector(`[data-value='=']`)
+		} else {
+			keysClickedItem = document.querySelector(`[data-value='${keysItemVDatasetValue}']`)
+		}
+		if (keysClickedItem == null) return
+
+		// BLOCK ONLY ZEROS IN CALC
+		if (currentValue == '0' && keysItemVDatasetValue == '0') {
+			return
+		} else if (currentValue == '0' && keysClickedItem.dataset.type == 'Number') {
+			currentValue = ''
+		}
+
+		currentValue = currentValue.toString()
+
+		if (keysClickedItem.dataset.type == 'Number' && currentValue.length < 15 && currentValue !== "Can't divide by 0!") {
+			currentValue == endResult ? (currentValue = '') : false
+			currentValue = currentValue + keysItemVDatasetValue.toString()
+		} else if (currentValue != '' && keysClickedItem.dataset.type == 'Solo') {
+			makeAnOperation(keysItemVDatasetValue)
+		} else if (
+			keysItemVDatasetValue == '.' &&
+			!currentValue.includes('.') &&
+			currentValue !== '' &&
+			currentValue !== "Can't divide by 0!"
+		) {
+			currentValue = currentValue + keysItemVDatasetValue.toString()
+		} else if (keysItemVDatasetValue == 'Backspace' && currentValue !== "Can't divide by 0!") {
+			currentValue = currentValue.slice(0, -1)
+		} else if (keysItemVDatasetValue == 'Del') {
+			currentValue = ''
+		} else if (keysItemVDatasetValue == 'DelAll') {
+			currentValue = ''
+			prevValue = ''
+			chosenOperation = ''
+		} else if (keysItemVDatasetValue == 'Enter' && chosenOperation != '') {
+			makeAnOperation(keysItemVDatasetValue)
+		}
+		console.log(keysItemVDatasetValue);
+
+		chosenOperationFunction(keysItemVDatasetValue, keysClickedItem.dataset.type)
+		writeOnDisplay()
+	}
+
 	calculatorBox.addEventListener('click', manageCalculatorClick)
-	// document.addEventListener('keyup', manageKeys)
+	document.addEventListener('keyup', manageKeys)
 }
