734d7e55c718a375c3e8dc335d3e6393eb1f2a46
Tomasz Ż <tzuber95@gmail.com>
2022-11-03 19:08:45 +0100

    ended weather app

--- src/js/_weather.js
+++ src/js/_weather.js
@@ -16,11 +16,10 @@ export const weatherAppFunction = () => {
 	const API_KEY = '&appid=1503629039d232e2a67ca14492858f91'
 	const API_UNITS = '&units=metric'
 	let weatherData
+	let cookiesCity = localStorage.getItem('city')
 
 	// GET DATA FROM API
-	const getAPIWeather = () => {
-		const cityName = weatherInput.value || 'Kielce'
-
+	const getAPIWeather = cityName => {
 		const API_URL = API_LINK + cityName + API_KEY + API_UNITS
 
 		axios
@@ -31,68 +30,144 @@ export const weatherAppFunction = () => {
 					cityTemp: response.data.main.temp.toFixed(),
 					cityPress: response.data.main.pressure,
 					cityHum: response.data.main.humidity,
-					citySunrise: response.data.sys.sunrise,
-					citySunset: response.data.sys.sunset,
-					cityWeatherIcon: response.data.weather[0].icon,
+					citySunrise: response.data.sys.sunrise * 1000,
+					citySunset: response.data.sys.sunset * 1000,
+					cityWeatherID: response.data.weather[0].id,
 					cityWeatherDesc: response.data.weather[0].main,
 					cityWindSpeed: response.data.wind.speed.toFixed(1),
 					cityWindDeg: response.data.wind.deg,
 					cityTimezoneSeconds: response.data.timezone * 1000,
 				}
-
+				
 				manageDataFromAPI(weatherData)
+				localStorage.setItem('city', cityName)
+				weatherInputErr.style.display = 'none'
+				weatherInput.value = ''
 			})
 			.catch(() => (weatherInputErr.style.display = 'block'))
 	}
 
+	// FUNCTION CHANGING LABELS, SPAN AND CLOCK
 	const manageDataFromAPI = weather => {
-		const iconURL = 'http://openweathermap.org/img/wn/'
-		const endOfIconUrl = '@2x.png'
-		console.log(weather)
-		weatherLabel.textContent = weather.cityName
 		weatherSpan.textContent = weather.cityWeatherDesc
 		tempSpan.textContent = `${weather.cityTemp} °C`
 		pressSpan.textContent = `${weather.cityPress} hPa`
 		humSpan.textContent = `${weather.cityHum} %`
 		windSpan.textContent = `${weather.cityWindSpeed} m/s`
 		windArrow.style.transform = `rotate(${weather.cityWindDeg}deg)`
-		weatherImg.setAttribute('src', `${iconURL}${weather.cityWeatherIcon}${endOfIconUrl}`)
 
-		const sunriseDate = new Date(weather.citySunrise - weather.cityTimezoneSeconds)
+		cityNameInLabel(weather.cityName)
+		setProperIcon(weather.cityWeatherID)
+		getCurrentTime(weather.cityTimezoneSeconds)
+		sunriseAndSunsetClock(weather.citySunrise, weather.citySunset, weather.cityTimezoneSeconds)
+	}
+
+	// BIG LETTERS IN CITY NAME
+	let cityNameInLabel = cityName => {
+		const bigLetters = cityName
+			.split(' ')
+			.map(cityName => (cityName = cityName.charAt(0).toUpperCase() + cityName.substring(1).toLowerCase()))
+			.join(' ')
+
+		weatherLabel.textContent = bigLetters
+	}
+
+	// SET ICON FOR WEATHER
+	const setProperIcon = id => {
+		if (id >= 200 && id < 300) {
+			weatherImg.setAttribute('src', './src/img/thunderstorm.png')
+		} else if (id >= 300 && id < 400) {
+			weatherImg.setAttribute('src', './src/img/drizzle.png')
+		} else if (id >= 500 && id < 600) {
+			weatherImg.setAttribute('src', './src/img/rain.png')
+		} else if (id >= 600 && id < 700) {
+			weatherImg.setAttribute('src', './src/img/ice.png')
+		} else if (id >= 700 && id < 800) {
+			weatherImg.setAttribute('src', './src/img/fog.png')
+		} else if (id === 800) {
+			weatherImg.setAttribute('src', './src/img/sun.png')
+		} else if (id > 800 && id < 900) {
+			weatherImg.setAttribute('src', './src/img/cloud.png')
+		} else {
+			weatherImg.setAttribute('src', './src/img/unknown.png')
+		}
+	}
+
+	// GET TIME IN CURRENT CITY
+	const getCurrentTime = cityTime => {
+		const nowTime = new Date()
+		const nowTimeMs = nowTime.getTime()
+		const currentTimeInCity = new Date(nowTimeMs + cityTime - 3600000)
+
+		const nowHours =
+			currentTimeInCity.getHours() < 10 ? `0${currentTimeInCity.getHours()}` : currentTimeInCity.getHours()
+		const nowMinutes =
+			currentTimeInCity.getMinutes() < 10 ? `0${currentTimeInCity.getMinutes()}` : currentTimeInCity.getMinutes()
+		const nowSeconds =
+			currentTimeInCity.getSeconds() < 10 ? `0${currentTimeInCity.getSeconds()}` : currentTimeInCity.getSeconds()
+
+		weatherTimer.textContent = `${nowHours}:${nowMinutes}:${nowSeconds}`
+	}
+
+	// SUNSET AND SUNRISE TIME
+	const sunriseAndSunsetClock = (sunrise, sunset, timezone) => {
+		const sunriseDate = new Date(sunrise + timezone - 3600000)
 		const sunriseHours = sunriseDate.getHours() < 10 ? `0${sunriseDate.getHours()}` : sunriseDate.getHours()
 		const sunriseMinutes = sunriseDate.getMinutes() < 10 ? `0${sunriseDate.getMinutes()}` : sunriseDate.getMinutes()
 		const sunriseSeconds = sunriseDate.getSeconds() < 10 ? `0${sunriseDate.getSeconds()}` : sunriseDate.getSeconds()
 
-		const sunsetDate = new Date(weather.citySunset)
+		const sunsetDate = new Date(sunset + timezone - 3600000)
 		const sunsetHours = sunsetDate.getHours() < 10 ? `0${sunsetDate.getHours()}` : sunsetDate.getHours()
 		const sunsetMinutes = sunsetDate.getMinutes() < 10 ? `0${sunsetDate.getMinutes()}` : sunsetDate.getMinutes()
 		const sunsetSeconds = sunsetDate.getSeconds() < 10 ? `0${sunsetDate.getSeconds()}` : sunsetDate.getSeconds()
 
 		sunriseSpan.textContent = `${sunriseHours}:${sunriseMinutes}:${sunriseSeconds}`
 		sunsetSpan.textContent = `${sunsetHours}:${sunsetMinutes}:${sunsetSeconds}`
-		weatherInputErr.style.display = 'none'
 	}
 
-	// GET CURRENT TIME
-	const getCurrentTime = () => {
-		const nowTime = new Date()
-		const nowHours = nowTime.getHours() < 10 ? `0${nowTime.getHours()}` : nowTime.getHours()
-		const nowMinutes = nowTime.getMinutes() < 10 ? `0${nowTime.getMinutes()}` : nowTime.getMinutes()
-		const nowSeconds = nowTime.getSeconds() < 10 ? `0${nowTime.getSeconds()}` : nowTime.getSeconds()
+	// CHANGE TIME IN CLOCK
+	const changeClock = () => {
+		const currentTime = weatherTimer.textContent
+		let currentTimeHours = currentTime.slice(0, 2)
+		let currentTimeMin = currentTime.slice(3, 5)
+		let currentTimeSec = currentTime.slice(-2)
 
-		weatherTimer.textContent = `${nowHours}:${nowMinutes}:${nowSeconds}`
+		if (currentTimeHours == 23 && currentTimeMin == 59 && currentTimeSec == 59) {
+			weatherTimer.textContent = `00:00:00`
+		} else if (currentTimeMin == 59 && currentTimeSec == 59) {
+			currentTimeHours++
+			if (currentTimeHours <= 9) {
+				weatherTimer.textContent = `0${currentTimeHours}:00:00`
+			} else {
+				weatherTimer.textContent = `${currentTimeHours}:00:00`
+			}
+		} else if (currentTimeSec == 59) {
+			currentTimeMin++
+			if (currentTimeMin <= 9) {
+				weatherTimer.textContent = `${currentTimeHours}:0${currentTimeMin}:00`
+			} else {
+				weatherTimer.textContent = `${currentTimeHours}:${currentTimeMin}:00`
+			}
+		} else if (currentTimeSec >= 9 && currentTimeSec < 59) {
+			currentTimeSec++
+			weatherTimer.textContent = `${currentTimeHours}:${currentTimeMin}:${currentTimeSec}`
+		} else if (currentTimeSec < 9) {
+			currentTimeSec++
+			weatherTimer.textContent = `${currentTimeHours}:${currentTimeMin}:0${currentTimeSec}`
+		}
 	}
 
 	// MANAGE ENTER
 	const weatherEnterManage = e => {
-		if (e.keyCode === 13) {
-			getAPIWeather()
-			weatherInput.value = ''
+		if (e.keyCode == 13 && weatherInput.value != '') {
+			const cityName = weatherInput.value
+			getAPIWeather(cityName)
+		} else if (e.keyCode == 13 && weatherInput.value == '') {
+			weatherInputErr.style.display = 'block'
 		}
 	}
 
 	document.addEventListener('keyup', weatherEnterManage)
-	getCurrentTime()
-	setInterval(getCurrentTime, 1000)
-	getAPIWeather()
+	getAPIWeather(cookiesCity || 'London')
+	setInterval(changeClock, 1000)
 }
